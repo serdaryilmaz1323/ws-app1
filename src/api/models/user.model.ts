3163c745347cba8db542cc5659abe2679057d71b
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-03-26 12:39:04 +0100

    feat : create user model

---
+++ src/api/models/user.model.ts
@@ -0,0 +1,29 @@
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: UserAddress;
+  phone: string;
+  website: string;
+  company: UserCompany;
+}
+
+export interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: UserGeo;
+}
+
+export interface UserGeo {
+  lat: string;
+  lng: string;
+}
+
+export interface UserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
