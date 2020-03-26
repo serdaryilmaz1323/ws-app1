e30d7b6f05fd07e83c85f52b85b388242a776277
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-03-26 22:28:00 +0100

    feat: add loading

--- src/container/UserList/UserList.tsx
+++ src/container/UserList/UserList.tsx
@@ -1,32 +1,36 @@
 import './UserList.css';
 import React, { useState, useEffect } from 'react';
-import { IonGrid, IonRow } from '@ionic/react';
+import { IonList, IonGrid, IonRow } from '@ionic/react';
 import { IUser } from '../../api/models/user.model';
 import { UserService } from '../../api/services/user.service';
 import UserListItem from './components/UserListItem';
+import Loading from '../../components/Loading';
 
 type State = {
   userList: IUser[];
+  loading: boolean;
 };
 
-const initialState: State = { userList: [] };
+const initialState: State = { userList: [], loading: true };
 
 const UserList = () => {
   const [state, setState] = useState<State>(initialState);
 
   useEffect(() => {
     UserService.fetchUserList().then(response => {
-      setState({ userList: response });
+      setState({ userList: response, loading: false });
       console.log(response);
     });
   }, []);
 
-  return (
+  return state.loading ? (
+    <Loading />
+  ) : (
     <IonGrid>
       <IonRow>
-        {state.userList.map(item => {
-          return <UserListItem key={item.id} user={item} />;
-        })}
+        {state.userList.map(item => (
+          <UserListItem key={item.id} user={item} />
+        ))}
       </IonRow>
     </IonGrid>
   );
