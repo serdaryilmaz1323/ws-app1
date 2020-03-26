5909728125c7cf3ea2c15e1b2f04b7a1529e4733
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-03-26 21:21:52 +0100

    feat: add todo, post, album pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Redirect, Route, BrowserRouter } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -22,14 +21,18 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import UserPage from './pages/UserPage/UserPage';
+import TodoPage from './pages/TodoPage';
+import PostPage from './pages/PostPage';
+import AlbumPage from './pages/AlbumPage';
 
 const App: React.FC = () => (
   <IonApp>
     {/* <IonReactRouter> */}
     <IonRouterOutlet>
       <BrowserRouter>
-        <Route path="/user/:id" component={UserPage} />
+        <Route path="/user/:id/todos" component={TodoPage} />
+        <Route path="/user/:id/posts" component={PostPage} />
+        <Route path="/user/:id/albums" component={AlbumPage} />
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
         {/* <Redirect to="/error" /> */}
