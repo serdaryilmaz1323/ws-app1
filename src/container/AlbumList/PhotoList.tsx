e5e8b92ba8a42db99bbc4a12de6aaff11c8f209d
Serdar YILMAZ <serdaryilmaz1323@gmail.com>
2020-03-27 14:43:31 +0100

    feat: add view options (grid/slide)

--- src/container/AlbumList/PhotoList.tsx
+++ src/container/AlbumList/PhotoList.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { IPhoto } from '../../api/models/album.model';
-import { IonGrid, IonRow, IonCol, IonContent } from '@ionic/react';
+import { IonGrid, IonRow, IonCol, IonContent, IonSlides, IonSlide } from '@ionic/react';
 //import { UserService } from '../../api/services/user.service';
 //import { TodoService } from '../../api/services/todo.service';
 import { AlbumService } from '../../api/services/album.service';
+import './PhotoList.css';
 
 type Props = { albumId: number };
 type State = {
   photoList: IPhoto[];
   loading: boolean;
+  view: 'grid' | 'slide';
 };
 const PhotoList = (props: Props) => {
-  const [state, setState] = useState<State>({ loading: true, photoList: [] });
+  const [state, setState] = useState<State>({ loading: true, photoList: [], view: 'slide' });
 
   useEffect(() => {
     let cancel = false;
@@ -28,8 +30,8 @@ const PhotoList = (props: Props) => {
     //eslint-disable-next-line
   }, []);
 
-  return (
-    <IonContent>
+  return state.view === 'grid' ? (
+    <IonContent scroll-y={false}>
       <IonGrid>
         <IonRow>
           {state.photoList.map(p => (
@@ -39,6 +41,19 @@ const PhotoList = (props: Props) => {
           ))}
         </IonRow>
       </IonGrid>
+    </IonContent>
+  ) : (
+    <IonContent scroll-y={false}>
+      <IonSlides pager={true}>
+        {state.photoList.map(p => (
+          <IonSlide key={p.id}>
+            <div className="slide">
+              <img alt={p.title} src={p.url} />
+              <p>{p.title}</p>
+            </div>
+          </IonSlide>
+        ))}
+      </IonSlides>
     </IonContent>
   );
 };
